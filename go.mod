f97b5f670002ba5d7a6b1b9baace638abdb48c86
makachanm <hyeonseongyang09@gmail.com>
2025-10-06 23:08:42 +0900

    add

---
+++ go.mod
@@ -0,0 +1,8 @@
+module tunefrags
+
+go 1.25.1
+
+require (
+	github.com/goccy/go-yaml v1.18.0
+	github.com/gomarkdown/markdown v0.0.0-20250810172220-2e2c11897d1a
+)
---
+++ main.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"html/template"
+	"os"
+	"slices"
+	"strings"
+
+	"github.com/goccy/go-yaml"
+	"github.com/gomarkdown/markdown"
+	"github.com/gomarkdown/markdown/html"
+	"github.com/gomarkdown/markdown/parser"
+)
+
+type Post struct {
+	Author  string `yaml:"author"`
+	Title   string `yaml:"title"`
+	Content template.HTML
+}
+
+func LoadFromMarkdownFile(filename string) Post {
+	raw, err := os.ReadFile(filename)
+	if err != nil {
+		panic(err)
+	}
+
+	poststring := string(raw)
+	splited := strings.Split(poststring, "==========")
+
+	var postinfo Post
+	err = yaml.Unmarshal([]byte(splited[0]), &postinfo)
+
+	if err != nil {
+		panic(err)
+	}
+
+	p := parser.NewWithExtensions(parser.CommonExtensions)
+	asts := p.Parse([]byte(splited[1]))
+
+	r := html.NewRenderer(html.RendererOptions{Flags: html.CommonFlags | html.HrefTargetBlank})
+	postinfo.Content = template.HTML(markdown.Render(asts, r))
+
+	return postinfo
+}
+
+func RenderToHTMLTemplates(p Post) {
+	t, err := template.ParseFiles("templates/post.html")
+	if err != nil {
+		panic(err)
+	}
+
+	f, err := os.Create("dist/" + p.Title + ".html")
+	if err != nil {
+		panic(err)
+	}
+
+	err = t.Execute(f, p)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func LoadAllPosts() []Post {
+	files, err := os.ReadDir("posts")
+	if err != nil {
+		panic(err)
+	}
+
+	var posts []Post
+	for _, file := range files {
+		if !file.IsDir() {
+			posts = append(posts, LoadFromMarkdownFile("posts/"+file.Name()))
+		}
+	}
+
+	return posts
+}
+
+func RenderIndex(posts []Post) {
+	slices.Reverse(posts)
+	t, err := template.ParseFiles("templates/index.html")
+	if err != nil {
+		panic(err)
+	}
+
+	f, err := os.Create("dist/index.html")
+	if err != nil {
+		panic(err)
+	}
+
+	err = t.Execute(f, posts)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	posts := LoadAllPosts()
+	for _, post := range posts {
+		RenderToHTMLTemplates(post)
+	}
+	RenderIndex(posts)
+}
